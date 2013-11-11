65cdf43b1187e337ab011f046e3e24d981f6c704
Alwin Egger <alwin.egger@tegonal.com>
2013-11-11 14:58:53 +0100

    using play 2.2.1 and play-slick 0.5.0.8

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,16 @@ object ApplicationBuild extends Build {
   val appVersion = "1.0-SNAPSHOT"
 
   val nscalatime = "com.github.nscala-time" %% "nscala-time" % "0.4.2"
+  val slick = "com.typesafe.play" %% "play-slick" % "0.5.0.8" 
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
     cache,
-    nscalatime)
+    nscalatime,
+    slick)
 
   val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
-  ).dependsOn(RootProject(uri("git://github.com/freekh/play-slick.git")))
+  )
 
 }
